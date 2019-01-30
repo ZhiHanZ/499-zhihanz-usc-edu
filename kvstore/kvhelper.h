d6d58cfead6a10c1976842917ab36dacc593c9f6
vagrant <vagrant@vagrant.vm>
2019-01-30 01:36:23 +0000

    fixed tab

--- kvstore/kvhelper.h
+++ kvstore/kvhelper.h
@@ -6,28 +6,28 @@ using namespace std;
 //helper methods used in key value store
 namespace helper {
 
-	int64_t Put(ConcurrentHashTable<string, string>& table
-			        , const string& key, const string& value){
-		if(table.Has(key)){
-			return -1;
-		}	else {
-			table.Add(key, value);
-			return 0;
-		}
-	}	
+  int64_t Put(ConcurrentHashTable<string, string>& table
+              , const string& key, const string& value){
+    if(table.Has(key)){
+      return -1;
+    }	else {
+      table.Add(key, value);
+      return 0;
+    }
+  }	
 
-	string Get(ConcurrentHashTable<string, string>& table
-					  , const string& key){
-		return table.GetValue(key);
-	}
+  string Get(ConcurrentHashTable<string, string>& table
+             , const string& key){
+    return table.GetValue(key);
+  }
 
 	int64_t Delete(ConcurrentHashTable<string, string>& table
-			          , const string& key){
-		if(table.Has(key)){
-			table.DeleteKey(key);
-			return 0;
-		}else {
-			return -1;
-		}
-	}
+                 , const string& key){
+    if(table.Has(key)){
+      table.DeleteKey(key);
+      return 0;
+    }else {
+      return -1;
+    }
+  }
 }
