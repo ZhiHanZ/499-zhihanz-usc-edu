087714add1544d3b27217a4b3331868fb3a0c2ab
vagrant <vagrant@vagrant.vm>
2019-01-30 01:20:00 +0000

    fixed

--- kvstore/cchash.h
+++ kvstore/cchash.h
@@ -11,10 +11,10 @@ template<typename Key, typename Value, typename Hash=std::hash<Key>>
 class ConcurrentHashTable {
   public: 
     //Consstruct a hash table with its default size is 19.
-    ConcurrentHashTable(unsigned int num_buckets = default_buckets_)
+    explicit ConcurrentHashTable(unsigned num_buckets = default_buckets_)
 			:table_(num_buckets){
       for (unsigned i = 0; i < num_buckets; ++i) {
-	table_[i].reset(new bucket_type);
+				table_[i].reset(new BucketType);
       }
     }
     //Get value corresponding to the key;
@@ -49,63 +49,62 @@ class ConcurrentHashTable {
     {
       public:
         bool HasKey(const Key& key){
-	  std::shared_lock<std::shared_mutex> lock(mutex);
-	  return (FindEntry(key) == data_.end())? false: true;
-	 }
-	 //pre: Has Key in hash table
-	 Value GetValue(const Key& key){
-	   std::shared_lock<std::shared_mutex> lock(mutex);
-	   return FindEntry(key)->second;
-	 }
-	 Value GetValue(const Key& key, const Key& default_val){
-	   std::shared_lock<std::shared_mutex> lock(mutex);
-	   return (FindEntry(key) == data_.end())? default_val: FindEntry(key)->second;
-	 }
-	 bool AddValue(const Key& key, const Value& value) {
-	   std::unique_lock<std::shared_mutex> lock(mutex);
-	   bucket_iter iter_ = FindEntry(key);
-	   if (iter_ == data_.end()) {
-	     data_.push_back(make_pair(key, value));
-	     return true;
-	   } 
-	   else {
-	     return false;
-	   }
-	 }
-	 void AddOrUpdateValue(const Key& key, const Value& value){
-	   std::unique_lock<std::shared_mutex> lock(mutex);
-	   bucket_iter iter_ = FindEntry(key);
-	   if (iter_ != data_.end()) {
-	     iter_->second = value;
-	   } else {
-	     data_.push_back(make_pair(key, value));
-	   }
-	 }
-	 void DeleteKey(const Key& key) {
-	   std::unique_lock<std::shared_mutex> lock(mutex);
-	   if (FindEntry(key) != data_.end()) {
-	     data_.erase(FindEntry(key));
-	   }
-	 }
-       private:
-	typedef std::pair<Key, Value> bucket_value;
-	typedef std::list<bucket_value> bucket_data;
-	bucket_data data_;
-	typedef typename bucket_data::iterator bucket_iter;
-	mutable std::shared_mutex mutex; //c++ 17 feature
-	bucket_iter FindEntry(const Key& key){
-	  return std::find_if(data_.begin(), data_.end(), [&key](bucket_value \
-	  			const &item) {return item.first == key;});
-	}
-     };
-     std::vector<std::unique_ptr<bucket_type>> table_;
-     Hash hash_;
-     int64_t default_buckets_ = 19;
-     BucketType& GetBucket(const Key& key) const {
-       std::size_t const index_ = hash_(key)%table_.size();
-       return *table_[index_];
-     }
-   
+	 	      std::shared_lock<std::shared_mutex> lock(mutex);
+	        return (FindEntry(key) == data_.end())? false: true;
+	      }		
+			 //pre: Has Key in hash table
+			 Value GetValue(const Key& key){
+				 std::shared_lock<std::shared_mutex> lock(mutex);
+				 return FindEntry(key)->second;
+			 }
+			 Value GetValue(const Key& key, const Key& default_val){
+				 std::shared_lock<std::shared_mutex> lock(mutex);
+				 return (FindEntry(key) == data_.end())? default_val: FindEntry(key)->second;
+			 }
+			 bool AddValue(const Key& key, const Value& value) {
+				 std::unique_lock<std::shared_mutex> lock(mutex);
+				 bucket_iter iter_ = FindEntry(key);
+				 if (iter_ == data_.end()) {
+					 data_.push_back(make_pair(key, value));
+					 return true;
+				 } 
+				 else {
+					 return false;
+				 }
+			 }
+			 void AddOrUpdateValue(const Key& key, const Value& value){
+				 std::unique_lock<std::shared_mutex> lock(mutex);
+				 bucket_iter iter_ = FindEntry(key);
+				 if (iter_ != data_.end()) {
+					 iter_->second = value;
+				 } else {
+					 data_.push_back(make_pair(key, value));
+				 }
+			 }
+			 void DeleteKey(const Key& key) {
+				 std::unique_lock<std::shared_mutex> lock(mutex);
+				 if (FindEntry(key) != data_.end()) {
+					 data_.erase(FindEntry(key));
+				 }
+			 }
+     private:
+			typedef std::pair<Key, Value> bucket_value;
+			typedef std::list<bucket_value> bucket_data;
+			bucket_data data_;
+			typedef typename bucket_data::iterator bucket_iter;
+			mutable std::shared_mutex mutex; //c++ 17 feature
+			bucket_iter FindEntry(const Key& key){
+				return std::find_if(data_.begin(), data_.end(), [&key](bucket_value \
+							const &item) {return item.first == key;});
+			}
+  };
+  std::vector<std::unique_ptr<BucketType>> table_;
+  Hash hash_;
+  const static unsigned default_buckets_ = 19;
+  BucketType& GetBucket(const Key& key) const {
+	  std::size_t const index_ = hash_(key)%table_.size();
+	  return *table_[index_];
+  }
 };
 #endif //CC_HASH_H_
 
