a150c1b9b4217609ccb2598c067c53908b83a460
vagrant <vagrant@vagrant.vm>
2019-01-30 01:34:08 +0000

    removed tab

--- kvstore/kvstore.cc
+++ kvstore/kvstore.cc
@@ -28,49 +28,50 @@ ConcurrentHashTable<std::string, std::string> database;
 //support put(key, value), get(key), delete(key) operation
 //operate success return Status::OK
 class KeyValueStoreImp1 final : public KeyValueStore::Service {
-	Status put(ServerContext* context, const PutRequest* request, PutReply* reply) override {			
-		if (Put(database, request->key(), request->value()) == 0){
-			return Status::OK;
-		}
-		else {
-			return Status(StatusCode::ALREADY_EXISTS, "key exists.");
-		}
+  Status put(ServerContext* context, const PutRequest* request, PutReply* reply) override {			
+    if (Put(database, request->key(), request->value()) == 0) {
+      return Status::OK;
+    }
+    else {
+      return Status(StatusCode::ALREADY_EXISTS, "key exists.");
+    }
 	}
-	Status get(ServerContext* context, ServerReaderWriter<GetReply,GetRequest>* stream) override {
-		GetRequest request;
-		while (stream->Read(&request)){
-			GetReply response;
-			auto replyval = Get(database, request.key());
-			if(database.Has(request.key())){
-				response.set_value(replyval);
-				stream->Write(response);
-				return Status::OK;
-			}else {
-				return Status(StatusCode::ALREADY_EXISTS, "key does not exist.");
-			}
-		}	
-	}
-	Status deletekey (ServerContext* context, const DeleteRequest* request, DeleteReply* reply) override {
-		if(Delete(database, request->key()) == 0){
-			return Status::OK;
-		}else {
-			return Status(StatusCode::ALREADY_EXISTS, "key does not exist.");
-		}
-	}
+  Status get(ServerContext* context, 
+             ServerReaderWriter<GetReply,GetRequest>* stream) override {
+    GetRequest request;
+    while (stream->Read(&request)){   
+      GetReply response;
+      auto replyval = Get(database, request.key());
+      if(database.Has(request.key())){
+        response.set_value(replyval);
+        stream->Write(response);
+        return Status::OK;
+      }else {
+        return Status(StatusCode::ALREADY_EXISTS, "key does not exist.");
+      }
+    }	
+  }
+  Status deletekey (ServerContext* context, const DeleteRequest* request, DeleteReply* reply) override {
+    if(Delete(database, request->key()) == 0){
+      return Status::OK;
+    }else {
+      return Status(StatusCode::ALREADY_EXISTS, "key does not exist.");
+    }
+  }
 };
 
 void RunServer() {
-	std::string server_address{"0.0.0.0:50000"};
-	KeyValueStoreImp1 service;
-	ServerBuilder builder;
-	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-	builder.RegisterService(&service);
-	std::unique_ptr<Server> server(builder.BuildAndStart());
-	std::cout << "Server listening on " << server_address << std::endl;
-	server->Wait();
+  std::string server_address{"0.0.0.0:50000"};
+  KeyValueStoreImp1 service;
+  ServerBuilder builder; 
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
 }
 
 int main(int argc, char** argv) {
-	RunServer();
-	return 0;
+  RunServer();
+  return 0;
 }
