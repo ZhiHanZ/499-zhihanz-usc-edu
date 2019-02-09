ce0b212a0c7f59b73bffb313bedb669ff53574ca
vagrant <vagrant@vagrant.vm>
2019-02-09 22:10:57 +0000

    add comment on test

--- service/unittest_kvstore.cc
+++ service/unittest_kvstore.cc
@@ -6,11 +6,14 @@
 #include "kvstore_client.h"
 #include "../kvstore/cchash.h"
 #include"kvstore.pb.h"
+
 using namespace UnitTest;
 using chirp::GetReply;
 using namespace std;
+
 ConcurrentHashTable<string, string> table{};
 UnitTestKVClient client{};
+//test that whether Put can put some key value pair into key value store.
 TEST(test, Put){
   auto b1 = client.Put("test1", "val1");
   auto b2 = client.Put("test1", "val2");
@@ -19,7 +22,7 @@ TEST(test, Put){
   auto val = client.GetValue("test1");
   EXPECT_EQ("val1", val);
 }
-
+//PutOrUpdate should add key value pair or update key value pair
 TEST(test, Update){
   auto b1 = client.PutOrUpdate("test1", "val2");
   auto b2 = client.PutOrUpdate("test1", "val3");
@@ -28,6 +31,7 @@ TEST(test, Update){
   auto val = client.GetValue("test1");
   EXPECT_EQ("val3", val);
 }
+// Test on whether key value pair can be deleted in key value store
 TEST(test, Delete){
   auto b1 = client.Delete("test1");
   EXPECT_EQ(true, b1);
