0a41b057da8ac8ef49f2b4d86df6726e51259df1
vagrant <vagrant@vagrant.vm>
2019-02-09 23:47:00 +0000

    add comments

--- service/kvstore_client.h
+++ service/kvstore_client.h
@@ -26,6 +26,14 @@ using chirp::KeyValueStore;
 using grpc::StatusCode;
 using grpc::Status;
 
+//KeyValue Store Client is used by service layer to communicate with key value store database
+//Support 
+//Put or Update key value pair 
+//Put given key value pair API
+//Get Value through key operation
+//tell server whether key value store has given key
+//and delete key value pair through given in key value store
+//operations
 class KeyValueStoreClient {
  public:
   KeyValueStoreClient(std::shared_ptr<Channel> channel)
@@ -54,6 +62,7 @@ class KeyValueStoreClient {
                 << std::endl;
     }
   }
+  //Put or Update given key value pair to database
 	Status PutOrUpdate(std::string key, std::string value) {
 		PutRequest request;
 		request.set_key(key);
@@ -63,6 +72,8 @@ class KeyValueStoreClient {
 		Status status = stub_->put(&context, request, &reply);
     return status;
 	}
+  //Put the given key value pair to database
+  //If database has given key return StautusCode:;ALREADY_EXISTS status
 	Status Put(std::string key, std::string value) {
     if (Has(key).ok()) {return Status(StatusCode::ALREADY_EXISTS, "key exists");}
 		PutRequest request;
@@ -73,6 +84,7 @@ class KeyValueStoreClient {
 		Status status = stub_->put(&context, request, &reply);
     return status;
 	}
+  //Get the value through key in key value store
   std::string GetValue(const std::string& key) {
     ClientContext context;
     auto stream = stub_->get(&context);
@@ -86,6 +98,7 @@ class KeyValueStoreClient {
     Status status = stream->Finish();
 		return response.value();
 	}
+  //Return the status shows that whether database has such key
   Status Has(std::string key) {
     ClientContext context;
     auto stream = stub_->get(&context);
@@ -99,6 +112,7 @@ class KeyValueStoreClient {
     Status status = stream->Finish();
     return status;
   }
+  //Delete a key value pair in databse through key
 	Status Delete(std::string key) {
 		DeleteRequest request;
 		request.set_key(key);
@@ -113,25 +127,33 @@ class KeyValueStoreClient {
 
 namespace UnitTest {
   using namespace std;
+  //The kvstore client which is used for unittest 
+  //Support Put, PutOrUpdate, GetValue through key, Delete key value pair through key operations
   class UnitTestKVClient {
     public:
       UnitTestKVClient() :table_{}{}
+      //Put or update the key value pair in key value store
       bool PutOrUpdate(std::string key, std::string value) {
         table_.AddOrUpdate(key, value);
         return true;
       }
+      //Put the key value pair into key value store
+      //If store has given key return false
       bool Put(std::string key, std::string value) {
         if(table_.Has(key)) return false;
         table_.Add(key, value);
         return true;
       }
+      //Get the value of corresponding key
       string GetValue(const std::string& key) {
         string response = table_.GetValue(key);
         return response;
       }
+      //return whether key value store has such key
       bool Has(std::string key) {
         return table_.Has(key);
       }
+      //delete corresponding key value pair through key
       bool Delete(std::string key) {
         table_.DeleteKey(key);
         return true;
