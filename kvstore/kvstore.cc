6884d88dfb2b3143f5396ddf2d816ffb12057a56
vagrant <vagrant@vagrant.vm>
2019-03-28 04:05:01 +0000

    kvstore

--- kvstore/kvstore.cc
+++ kvstore/kvstore.cc
@@ -4,10 +4,11 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
-#include "cchash.h"
-#include "kvhelper.h"
-#include "kvstore.grpc.pb.h"
-#include "kvstore.pb.h"
+#include "utils/cchash.h"
+#include "utils/kvhelper.h"
+#include "utils/pb/kvstore.grpc.pb.h"
+#include "utils/pb/kvstore.pb.h"
+#include "kvstore.h"
 
 using chirp::DeleteReply;
 using chirp::DeleteRequest;
@@ -22,31 +23,27 @@ using grpc::ServerContext;
 using grpc::ServerReaderWriter;
 using grpc::Status;
 using grpc::StatusCode;
-using helper::Delete;
-using helper::Get;
-using helper::Put;
-// store all variables in database
-ConcurrentHashTable<std::string, std::string> database;
-// support put(key, value), get(key), delete(key) operation
-// operate success return Status::OK
-class KeyValueStoreImp1 final : public KeyValueStore::Service {
+using key_val::helper::Delete;
+using key_val::helper::Get;
+using key_val::helper::Put;
+namespace key_val {
   // put given element in key value store do not allow to put existed key
-  Status put(ServerContext *context, const PutRequest *request,
-             PutReply *reply) override {
-    if (Put(database, request->key(), request->value()) == 0) {
+  Status KeyValueStoreImp1::put(ServerContext *context, const PutRequest *request,
+             PutReply *reply) {
+    if (Put(database_, request->key(), request->value()) == 0) {
       return Status::OK;
     } else {
       return Status(StatusCode::ALREADY_EXISTS, "key exists.");
     }
   }
   // get value through key, if key do not exists return error
-  Status get(ServerContext *context,
-             ServerReaderWriter<GetReply, GetRequest> *stream) override {
+  Status KeyValueStoreImp1::get(ServerContext *context,
+             ServerReaderWriter<GetReply, GetRequest> *stream) {
     GetRequest request;
     while (stream->Read(&request)) {
       GetReply response;
-      auto replyval = Get(database, request.key());
-      if (database.Has(request.key())) {
+      auto replyval = Get(database_, request.key());
+      if (database_.Has(request.key())) {
         response.set_value(replyval);
         stream->Write(response);
         return Status::OK;
@@ -57,20 +54,20 @@ class KeyValueStoreImp1 final : public KeyValueStore::Service {
     return Status::OK;
   }
   // delete a key . if key do not exists, return a error
-  Status deletekey(ServerContext *context, const DeleteRequest *request,
-                   DeleteReply *reply) override {
-    if (Delete(database, request->key()) == 0) {
+  Status KeyValueStoreImp1::deletekey(ServerContext *context, const DeleteRequest *request,
+                   DeleteReply *reply) {
+    if (Delete(database_, request->key()) == 0) {
       return Status::OK;
     } else {
       return Status(StatusCode::ALREADY_EXISTS, "key does not exist.");
     }
   }
-};
+}  // namespace key_val
 
 void RunServer() {
   std::string server_address{
       "0.0.0.0:50000"};  // this port is used as key value store
-  KeyValueStoreImp1 service;
+  key_val::KeyValueStoreImp1 service;
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
